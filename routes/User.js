f00f287c2488239cf9ff1516f0812514d1b0669e
Laura Perera <lauraperera.pss@gmail.com>
2022-03-25 17:24:07 -0300

    Atualizei a rota User com os métodos get e delete

--- routes/User.js
+++ routes/User.js
@@ -3,7 +3,11 @@ const express = require("express")
 //instance of a router
 const router = express.Router()
 
-router.get("/", controller.get)
+const {GetAllUsers, GetUser, DeleteUser} = require("../controllers/UserController");
+
+router.get("/all", GetAllUsers);
+router.get("/byId/:id", GetUser);
+router.delete("/:id", DeleteUser);
 
 //exports the router
 module.exports = router;
